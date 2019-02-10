db72ba47e526b7045bd55a92f2bdadec00c5f6f8
Kartik Prabhu <kprabhu7@stanford.edu>
2019-02-10 13:35:40 -0800

    Added DMA functionality to CGRA

--- src/main/scala/CGRASoC/CGRA.scala
+++ src/main/scala/CGRASoC/CGRA.scala
@@ -10,66 +10,105 @@ import freechips.rocketchip.regmapper.{HasRegMap, RegField}
 import freechips.rocketchip.tilelink._
 import freechips.rocketchip.util.UIntIsOneOf
 
-case class CGRAParams(address: BigInt, beatBytes: Int)
+class CGRA(address: BigInt, beatBytes: Int)(implicit p: Parameters) extends LazyModule {
+  val device = new SimpleDevice("cgra", Seq("ee272,cgra"))
 
-class CGRABase(w: Int) extends Module {
-  // TODO fix IO
-  val io = IO(new Bundle {
-    val data_out = Output(UInt(w.W))
-    val done = Output(Bool())
-    val enable = Input(Bool())
-    val address_in = Input(UInt(w.W))
-    val data_in = Input(UInt(w.W))
-  })
+  val regnode = TLRegisterNode(
+    address = Seq(AddressSet(address, 0x3f)),
+    device = device,
+    beatBytes = beatBytes)
 
-  // TODO add CGRA functionality
-  io.done := io.enable
-  io.data_out := io.data_in + 1.U
+  val dmanode = TLClientNode(Seq(TLClientPortParameters(
+    Seq(TLClientParameters(
+      name = "cgra", sourceId = IdRange(0, 1))))))
+
+  lazy val module = new CGRAModuleImp(this)
 }
 
-trait CGRABundle extends Bundle {
-  val data_out = Output(UInt(64.W))
-  val done = Output(Bool())
-}
+class CGRAModuleImp(outer: CGRA) extends LazyModuleImp(outer) {
+  val (tl, edge) = outer.dmanode.out(0)
+  val w = 64
+  val beatBytes = (edge.bundle.dataBits / 8)
 
-trait CGRAModule extends HasRegMap {
-  val io: CGRABundle
-  implicit val p: Parameters
-  def params: CGRAParams
-  
-  // TODO define registers
   val enable = RegInit(false.B)
-  val address_in = Reg(UInt(64.W))
-  val data_in = Reg(UInt(64.W))
+  val load_address = Reg(UInt(w.W))
+  val store_address = Reg(UInt(w.W))
+  val data_in = Reg(UInt(w.W))
+  val data_out = Reg(UInt(w.W))
+  val done = RegInit(false.B)
 
-  val base = Module(new CGRABase(64))
-  io.data_out := base.io.data_out
-  io.done := base.io.done
-  base.io.enable := enable
-  base.io.address_in := address_in
-  base.io.data_in := data_in
+  // state machine for CGRA
+  val s_idle :: s_fetch_1 :: s_fetch_2 :: s_exec_1 :: s_exec_2 :: s_wb_1 :: s_wb_2 :: Nil = Enum(7)
+  val state = RegInit(s_idle)
 
-  // TODO define regmap
-  regmap(
+  val get_req = edge.Get(
+    fromSource = 0.U,
+    toAddress = load_address,
+    lgSize = log2Ceil(beatBytes).U)._2
+  val put_req = edge.Put(
+    fromSource = 0.U,
+    toAddress = (store_address >> log2Ceil(beatBytes)-1) << log2Ceil(beatBytes)-1,
+    lgSize = log2Ceil(beatBytes).U,
+    data = data_out)._2
+
+  tl.a.valid := (state === s_fetch_1) || (state === s_wb_1)
+  tl.a.bits := Mux(state === s_fetch_1, get_req, put_req)
+
+  tl.d.ready := (state === s_fetch_2) || (state === s_wb_2)
+
+  when(state === s_idle && enable){ // CGRA has been enabled, go to fetch_1
+    state := s_fetch_1
+  }
+
+  when(state === s_fetch_1 && tl.a.fire()){ // get request has been sent
+    state := s_fetch_2
+    printf("sent get request to addr: %x\n", load_address)
+  }
+
+  when(state === s_fetch_2 && tl.d.fire()){ // receieved response to get request
+    printf("received data: %x\n", tl.d.bits.data)
+    data_in := tl.d.bits.data
+    state := s_exec_1
+  }
+
+  when(state === s_exec_1){
+    data_out := data_in + 1.U // perform trivial operation
+    state := s_exec_2
+    assert(store_address(log2Ceil(beatBytes)-1,0) === 0.U,
+    s"Store address not aligned to ${beatBytes} bytes")
+  }
+
+  when(state === s_exec_2){
+    state := s_wb_1
+  }
+
+  when(state === s_wb_1 && tl.a.fire()){ // put request has been sent
+    state := s_wb_2
+    printf("sent put request to addr: %x with data: %x\n", (store_address >> log2Ceil(beatBytes)-1) << log2Ceil(beatBytes)-1, data_out)
+  }
+
+  when(state === s_wb_2 && tl.d.fire()){ // received response to put request
+    printf("received response to put\n")
+    state := s_idle
+    done := true.B
+    enable := false.B
+  }
+
+  outer.regnode.regmap(
     0x00 -> Seq(
       RegField(1,enable)),
     0x08 -> Seq(
-      RegField(64, address_in)),
+      RegField(w, load_address)),
     0x10 -> Seq(
-      RegField(64, data_in)),
+      RegField(w, store_address)),
     0x18 -> Seq(
-      RegField(64, io.data_out)),
+      RegField(w, data_in)),
     0x20 -> Seq(
-      RegField(1, io.done))
+      RegField(w, data_out)),
+    0x28 -> Seq(
+      RegField(1, done)),
   )
 }
-
-class CGRATL(c: CGRAParams)(implicit p: Parameters)
-  extends TLRegisterRouter(
-    c.address, "cgra", Seq("cgra"),
-    beatBytes = c.beatBytes)(
-      new TLRegBundle(c, _) with CGRABundle)(
-      new TLRegModule(c, _, _) with CGRAModule)
 
 trait HasPeripheryCGRATL { this: BaseSubsystem =>
   implicit val p: Parameters
@@ -77,20 +116,13 @@ trait HasPeripheryCGRATL { this: BaseSubsystem =>
   private val address = 0x4000
   private val portName = "cgra"
 
-  val cgra = LazyModule(new CGRATL(
-    CGRAParams(address, pbus.beatBytes))(p))
+  val cgra = LazyModule(new CGRA(address, pbus.beatBytes)(p))
 
-  pbus.toVariableWidthSlave(Some(portName)) { cgra.node }
+  pbus.toVariableWidthSlave(Some(portName)) { cgra.regnode }
+  sbus.fromPort(Some(portName))() := cgra.dmanode
 }
 
 trait HasPeripheryCGRATLModuleImp extends LazyModuleImp {
   implicit val p: Parameters
   val outer: HasPeripheryCGRATL
-
-  val cgra_done = IO(Output(Bool()))
-  val cgra_data_out = IO(Output(UInt(64.W)))
-
-  cgra_done := outer.cgra.module.io.done 
-  cgra_data_out := outer.cgra.module.io.data_out
-  
 }
--- tests/soc.c
+++ tests/soc.c
@@ -5,17 +5,19 @@
 //#define PHY_DATA_IN 0x2020
 #define PHY_DATA_IN 0x2018
 
-#define CGRA_ENABLE 0x4030
-#define CGRA_ADDRESS_IN 0x4008
-#define CGRA_DATA_IN 0x4010
-#define CGRA_DATA_OUT 0x4018
-#define CGRA_DONE 0x4020
+#define CGRA_ENABLE 0x4000
+#define CGRA_LOAD_ADDRESS 0x4008
+#define CGRA_STORE_ADDRESS 0x4010
+#define CGRA_LOAD_DATA 0x4018
+#define CGRA_STORE_DATA 0x4020
+#define CGRA_DONE 0x4028
 
 #include <stdio.h>
 #include <time.h>
 #include "minunit.h"
 #include "mmio.h"
 
+uint64_t write_data[1];
 
 MU_TEST(phy_unit_test){
 
@@ -54,20 +56,28 @@ MU_TEST_SUITE(PHY_test_suite){
 }
 
 MU_TEST(cgra_unit_test){
+	#define INITIAL_DATA 0x01234567
+	write_data[0] = INITIAL_DATA;
+	reg_write64(CGRA_LOAD_ADDRESS, write_data);
+	reg_write64(CGRA_STORE_ADDRESS, write_data);
+
+	asm volatile ("fence");
+
+	uint64_t load_address = reg_read64(CGRA_LOAD_ADDRESS);
+	mu_check(load_address == write_data);
+
+	uint64_t store_address = reg_read64(CGRA_STORE_ADDRESS);
+	mu_check(store_address == write_data);
+
 	reg_write32(CGRA_ENABLE, 1);
-	uint32_t enable = reg_read32(CGRA_ENABLE);
+
+	asm volatile ("fence");
+
+volatile	uint32_t enable = reg_read32(CGRA_ENABLE);
 	mu_check(enable == 1);
-	uint32_t d = reg_read32(CGRA_DONE);
-	mu_check(d == 1);
 
-	uint64_t data = 0x01234567;
-	reg_write64(CGRA_ADDRESS_IN, &data);
-	uint64_t address = reg_read64(CGRA_ADDRESS_IN);
-	mu_check(address == &data);
-
-	
 	uint32_t done;
-	uint8_t timer = 1;
+	uint16_t timer = 1;
 	while( (done = reg_read32(CGRA_DONE)) == 0){
 		if(++timer == 0){
 			mu_fail("timed out!");
@@ -75,11 +85,14 @@ MU_TEST(cgra_unit_test){
 		}
 	}
 	
-	uint64_t data_in = reg_read64(CGRA_DATA_IN);
-	mu_check(data_in == data);
+	uint64_t data_in = reg_read64(CGRA_LOAD_DATA);
+	mu_check(data_in == INITIAL_DATA);
 
-	uint64_t data_out = reg_read64(CGRA_DATA_OUT);
-	mu_check(data_out == data + 1);
+	uint64_t data_out = reg_read64(CGRA_STORE_DATA);
+	mu_check(data_out == INITIAL_DATA + 1);
+	
+	printf("%016lx\n", write_data[0]);
+	mu_check(write_data[0] == INITIAL_DATA + 1);
 }
 
 MU_TEST_SUITE(CGRA_test_suite){
@@ -93,12 +106,8 @@ MU_TEST(integration_test){
 	uint32_t enable = reg_read32(PHY_ENABLE);
 	mu_check(enable == 1);
 
-
-	uint32_t done = 0;
-	while (done == 0){
-		done = reg_read32(PHY_DONE);
-	}
-
+	reg_write32(CGRA_LOAD_ADDRESS, PHY_DATA_OUT);
+	reg_write32(CGRA_ENABLE, 1);
 	uint32_t ld_addr = reg_read32(CGRA_ADDRESS_IN);
 	uint32_t st_addr = reg_read32(CGRA_DATA_IN);
 	uint32_t cgra_en = reg_read32(CGRA_ENABLE);
@@ -112,6 +121,7 @@ MU_TEST(integration_test){
 	mu_check(cgra_en == 1);
 
 	uint32_t phy_data = reg_read32(PHY_DATA_OUT);
+
 }
 
 MU_TEST_SUITE(integration_test_suite){
